cabf192c33214ce90dd5b874d395795f13dc24ee
İsmail Fatih Birak <ismailfatihbirak@gmail.com>
2024-05-05 09:50:25 +0300

    third

--- app/src/main/java/com/example/loodoscryptoapp/data/work_manager/CryptoState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.loodoscryptoapp.data.work_manager
-
-import com.example.loodoscryptoapp.domain.model.Root
-
-data class CryptoState(
-    val isLoading : Boolean = false,
-    val cryptos : List<Root> = listOf(),
-    val error : String = "",
-)
